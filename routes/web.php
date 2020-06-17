de5e59dbe8a9bc0eb117b473a78d26f226953500
Abyl <qwsdoam@gmail.com>
2020-06-17 14:06:52 +0500

    forget path auth

--- routes/web.php
+++ routes/web.php
@@ -71,5 +71,5 @@ Route::get('/logout', function(){
     Auth::logout();
     return Redirect::to('login');
 })->name('logout');
-Route::get('reset','ResetPasswordController@show');
-Route::post('resetpassword','ResetPasswordController@reset')->name('resetpassword');
+Route::get('reset','Auth\ResetPasswordController@show');
+Route::post('resetpassword','Auth\ResetPasswordController@reset')->name('resetpassword');
