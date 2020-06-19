96c567397b5e0e6cee0e5b81822619bcb03e97fe
Abyl <qwsdoam@gmail.com>
2020-06-19 16:17:32 +0500

    fix

--- resources/views/pages/courses.blade.php
+++ resources/views/pages/courses.blade.php
@@ -17,7 +17,7 @@
                 <div class="col-sm-8">
                     <div class="page-header float-right">
                         <div class="page-title">
-                            <ol class=" text-right">
+                            <ol class="brds text-right">
                                 <li><a href="/">Басты бет</a></li>
                                 <li>{{$listCourse->title}}</li>
 
--- resources/views/pages/mycourse.blade.php
+++ resources/views/pages/mycourse.blade.php
@@ -13,7 +13,7 @@
                 <div class="col-sm-8">
                     <div class="page-header float-right">
                         <div class="page-title">
-                            <ol class="breadcrumb text-right">
+                            <ol class="brds text-right">
                                 <li><a href="/">Басты бет</a></li>
 
 
