e38e5c1971d793a6dd4fcb5617b846ceff54a28f
Abyl <qwsdoam@gmail.com>
2020-06-21 15:01:50 +0500

    fix that

--- resources/views/pages/list-course.blade.php
+++ resources/views/pages/list-course.blade.php
@@ -15,7 +15,7 @@
                     <div class="page-header float-right">
                         <div class="page-title">
                             <ol class="breadcrumb text-right" style="text-transform:none">
-                                <li><a href="#">Dashboard</a></li>
+                                <li><a href="{{ url('/') }}">Басты бет</a></li>
                                 <li><a href="{{ url('student/section/'.request()->sec_id) }}">{{ request()->section }}</a></li>
                                 <li class="active">{{$listCourse->title}}</li>
                             </ol>
--- resources/views/pages/more-course.blade.php
+++ resources/views/pages/more-course.blade.php
@@ -4,7 +4,7 @@
     <div class="breadcrumbs">
         <div class="breadcrumbs-inner">
             <div class="row m-0">
-                <div class="col-sm-4">
+                <div class="col-sm-8">
                     <div class="page-header float-left">
                         <div class="page-title">
                             <h1> {{ $more->title }}</h1>
@@ -53,7 +53,7 @@
 
                             </div>
                         </div>
-                        <div class="col-lg-7">
+                        <div class="col-lg-12">
                             <div class="card-body">
                                 <div class="col-lg-12">
                                     {{ htmlspecialchars_decode($more->description) }}
