accb1faa65b82d52bde6317c879bd30cd3db7d10
Abyl <qwsdoam@gmail.com>
2020-08-07 17:13:21 +0500

    fixed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,6 +9,7 @@ use App\Entity\UserLog;
 use App\UseCases\Section\SectionListService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Cookie;
 
 class StudentController extends Controller
 {
