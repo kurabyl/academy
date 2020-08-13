2c91b61330e8dc8605c4eb9967ae16438f7020da
Abyl <qwsdoam@gmail.com>
2020-08-14 00:28:53 +0500

    fixed

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -112,25 +112,25 @@ class CourseController extends Controller
 
     private  function checkSession()
     {
-
         $userIp  = \request()->ip();
         $logExists = UserLog::where('user_id',Auth::user()->id);
 
-        if (!request()->cookie('cookieName')) {
-            UserLog::create([
-                'user_id'=>Auth::user()->id,
-                'session'=>request()->cookie('cookieName'),
-                'ip'=>$userIp,
-            ]);
+        if (\request()->cookie('cookieName') === $logExists->first()->session) {
+            return true;
         }else {
-            if($logExists->exists()) {
-                if($logExists->first()->session != request()->cookie('cookieName')) {
-                    return false;
+            if ($logExists->count() != 3) {
+                if(request()->cookie('cookieName')) {
+                    UserLog::create([
+                        'user_id' => Auth::user()->id,
+                        'session' => request()->cookie('cookieName'),
+                        'ip' => $userIp,
+                    ]);
                 }
+                return true;
+            }else {
+                return false;
             }
         }
-
-        return true;
 
     }
 
